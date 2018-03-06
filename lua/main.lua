e700db0a5e741aafc757a5385321e7523556da09
Janosch Kindl <kindl@users.noreply.github.com>
2018-03-06 16:14:07 +0100

    Watch changes

--- lua/main.lua
+++ lua/main.lua
@@ -1,11 +1,28 @@
-local Game = require "Spheres"
+local loadtime = 0
+local filename = "Game.lua"
+local game
 
-love.load = Game.load
+local watch = function(dt)
+    -- Checks if the loaded game file is newer
+    -- NOTE getLastModified will be deprecated with love 0.11
+    local modtime = love.filesystem.getLastModified(filename)
+    if modtime > loadtime then
+        game = love.filesystem.load(filename)()
+        if game then
+            for k, _ in pairs(game) do
+                if k ~= "update" then
+                    love[k] = game[k]
+                end
+            end
+            if game.load then game.load() end
+            loadtime = modtime
+            print("Loaded " .. filename)
+        else
+            print("Cannot load " .. filename)
+        end
+    else
+        if game.update then game.update(dt) end
+    end
+end
 
-love.draw = Game.draw
-
-love.update = Game.update
-
-love.mousemoved = Game.mousemoved
-
-love.mousepressed = Game.mousepressed
+love.update = watch

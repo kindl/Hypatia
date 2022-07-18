8b2d4ac66a4979306cc41871a7b8115dd51cc8ae
kindl <kindl@users.noreply.github.com>
2022-07-18 12:11:01 +0200

    Fixed operator precedence by passing previous

--- src/Operators.hs
+++ src/Operators.hs
@@ -24,40 +24,39 @@ fixAssoc operatorTable =
 captureAssocs (ModuleDeclaration _ decls) =
     fromList [(op, (assoc, prec)) | FixityDeclaration assoc prec op _ <- decls]
 
-fixAssocE operatorTable (InfixOperator (InfixOperator e1 child e2) root e3) =
-    fixAssocAux operatorTable LeftAssociative InfixOperator e1 child e2 root e3
-fixAssocE operatorTable (InfixOperator e1 root (InfixOperator e2 child e3)) =
-    fixAssocAux operatorTable RightAssociative InfixOperator e1 child e2 root e3
+fixAssocE operatorTable prev@(InfixOperator (InfixOperator e1 child e2) root e3) =
+    fixAssocAux operatorTable prev False InfixOperator e1 child e2 root e3
+fixAssocE operatorTable prev@(InfixOperator e1 root (InfixOperator e2 child e3)) =
+    fixAssocAux operatorTable prev True InfixOperator e1 child e2 root e3
 fixAssocE _ e = Right e
 
-fixAssocP operatorTable (PatternInfixOperator (PatternInfixOperator e1 child e2) root e3) =
-    fixAssocAux operatorTable LeftAssociative PatternInfixOperator e1 child e2 root e3
-fixAssocP operatorTable (PatternInfixOperator e1 root (PatternInfixOperator e2 child e3)) =
-    fixAssocAux operatorTable RightAssociative PatternInfixOperator e1 child e2 root e3
+fixAssocP operatorTable prev@(PatternInfixOperator (PatternInfixOperator e1 child e2) root e3) =
+    fixAssocAux operatorTable prev False PatternInfixOperator e1 child e2 root e3
+fixAssocP operatorTable prev@(PatternInfixOperator e1 root (PatternInfixOperator e2 child e3)) =
+    fixAssocAux operatorTable prev True PatternInfixOperator e1 child e2 root e3
 fixAssocP _ e = Right e
 
-fixAssocT operatorTable (TypeInfixOperator (TypeInfixOperator e1 child e2) root e3) =
-    fixAssocAux operatorTable LeftAssociative TypeInfixOperator e1 child e2 root e3
-fixAssocT operatorTable (TypeInfixOperator e1 root (TypeInfixOperator e2 child e3)) =
-    fixAssocAux operatorTable RightAssociative TypeInfixOperator e1 child e2 root e3
+fixAssocT operatorTable prev@(TypeInfixOperator (TypeInfixOperator e1 child e2) root e3) =
+    fixAssocAux operatorTable prev False TypeInfixOperator e1 child e2 root e3
+fixAssocT operatorTable prev@(TypeInfixOperator e1 root (TypeInfixOperator e2 child e3)) =
+    fixAssocAux operatorTable prev True TypeInfixOperator e1 child e2 root e3
 fixAssocT _ e = Right e
 
 -- General helper function for patterns, types and expressions
-fixAssocAux operatorTable prevAssoc constr e1 child e2 root e3 = do
+fixAssocAux operatorTable prev wasRightAssoc constr e1 child e2 root e3 = do
     (assoc1, prec1) <- findEither root operatorTable
     (assoc2, prec2) <- findEither child operatorTable
-    let left = constr (constr e1 root e2) child e3
-    let right = constr e1 child (constr e2 root e3)
-    case (compare prec1 prec2, prevAssoc, assoc1, assoc2) of
-        (EQ, LeftAssociative, RightAssociative, RightAssociative) -> Right right
-        (EQ, RightAssociative, LeftAssociative, LeftAssociative) -> Right left
-        (EQ, _, RightAssociative, LeftAssociative) -> fixAssocError child root
-        (EQ, _, LeftAssociative, RightAssociative) -> fixAssocError child root
-        (GT, LeftAssociative, _, _) -> Right right
-        (GT, RightAssociative, _, _) -> Right left
-        (_, LeftAssociative, _, _) -> Right left
-        (_, RightAssociative, _, _) -> Right right
-        _ -> fixAssocError child root
+    let switchedLeft = constr (constr e1 root e2) child e3
+    let switchedRight = constr e1 child (constr e2 root e3)
+    case (compare prec1 prec2, wasRightAssoc, assoc1, assoc2) of
+        (EQ, False, RightAssociative, RightAssociative) -> Right switchedRight
+        (EQ, True, LeftAssociative, LeftAssociative) -> Right switchedLeft
+        (EQ, _, RightAssociative, LeftAssociative) -> fixAssocError root assoc1 child assoc2
+        (EQ, _, LeftAssociative, RightAssociative) -> fixAssocError root assoc1 child assoc2
+        (GT, False, _, _) -> Right switchedRight
+        (GT, True, _, _) -> Right switchedLeft
+        (_, _, _, _) -> Right prev
 
-fixAssocError child root =
-    Left ("Cannot mix operator " ++ renderError child ++ " and " ++ renderError root)
+fixAssocError root assoc1 child assoc2 =
+    Left ("Cannot mix operator " ++ renderError root ++ " with " ++ show assoc1 ++ " and "
+        ++ renderError child ++ " with " ++ show assoc2)

b478d5fd0abf69f0d9d1d2072a6ee54ed795abcf
kindl <kindl@users.noreply.github.com>
2024-10-13 00:58:40 +0200

    Simplified love main

--- examples/lua/main.lua
+++ examples/lua/main.lua
@@ -1,27 +1,7 @@
-local loadtime = 0
-local filename = "Spheres.lua"
-local game
+local game = require "Example"
 
-local watch = function(dt)
-    -- Checks if the loaded game file is newer
-    local modtime = love.filesystem.getInfo(filename).modtime
-    if modtime > loadtime then
-        game = love.filesystem.load(filename)()
-        if game then
-            for k, _ in pairs(game) do
-                if k ~= "update" then
-                    love[k] = game[k]
-                end
-            end
-            if game.load then game.load() end
-            loadtime = modtime
-            print("Loaded " .. filename)
-        else
-            print("Cannot load " .. filename)
-        end
-    else
-        if game.update then game.update(dt) end
+for k, _ in pairs(game) do
+    if k ~= "update" then
+        love[k] = game[k]
     end
 end
-
-love.update = watch

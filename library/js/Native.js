bc1047c660ca594384d85d04eb10850751ac23eb
kindl <kindl@users.noreply.github.com>
2025-07-18 00:28:13 +0200

    Remove currying from js bindings

--- library/js/Native.js
+++ library/js/Native.js
@@ -1,85 +1,67 @@
 ﻿const Native = {};
 
-Native.eq = function (v1) {
-    return function (v2) {
-        return v1 === v2;
-    };
+Native.eq = function(v1, v2) {
+    return v1 === v2;
 };
 
-Native.multiply = function (v1) {
-    return function (v2) {
-        return v1 * v2;
-    };
+Native.multiply = function(v1, v2) {
+    return v1 * v2;
 };
 
-Native.divide = function (v1) {
-    return function (v2) {
-        return v1 / v2;
-    };
+Native.divide = function(v1, v2) {
+    return v1 / v2;
 };
 
-Native.modulo = function (v1) {
-    return function (v2) {
-        return v1 % v2;
-    };
+Native.modulo = function(v1, v2) {
+    return v1 % v2;
 };
 
-Native.plus = function (v1) {
-    return function (v2) {
-        return v1 + v2;
-    };
+Native.plus = function(v1, v2) {
+    return v1 + v2;
 };
 
-Native.minus = function (v1) {
-    return function (v2) {
-        return v1 - v2;
-    };
+Native.minus = function(v1, v2) {
+    return v1 - v2;
 };
 
-Native.concat = function (v1) {
-    return function (v2) {
-        return v1 + v2;
-    };
+Native.concat = function(v1, v2) {
+    return v1 + v2;
 };
 
-Native.negate = function (v) {
+Native.negate = function(v) {
     return -v;
 };
 
-Native.size = function (v) {
+Native.size = function(v) {
     return v.length;
 };
 
-Native.isArray = function (v) {
-    return typeof (v) === typeof ([]);
+Native.isArray = function(v) {
+    return typeof(v) === typeof([]);
 };
 
-Native.unsafeIndex = function (n) {
-    return function (a) {
-        return a[n];
-    };
+Native.unsafeIndex = function(n, a) {
+    return a[n];
 };
 
-Native.coerce = function (x) {
+Native.coerce = function(x) {
     return x;
 };
 
-Native.insert = function (a) {
-    return function (e) {
-        a.push(e);
-        return a;
-    };
+Native.insert = function(a, e) {
+    a.push(e);
+    return a;
 };
 
-Native.toString = function (x) {
+Native.toString = function(x) {
     return x.toString();
 };
 
-Native.print = function (x) {
+Native.print = function(x) {
     console.log(x);
 };
 
-Native.error = function (x) {
+Native.error = function(x) {
     throw x;
 };
 
